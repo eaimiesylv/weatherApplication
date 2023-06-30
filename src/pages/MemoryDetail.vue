ca88976e5800d25c499e0454cddd9abb90c2c5b5
eaimiesylv <okomemmanuel1@gmail.com>
2023-06-30 21:36:31 +0100

    worked on state management

--- src/pages/MemoryDetail.vue
+++ src/pages/MemoryDetail.vue
@@ -1,8 +1,9 @@
 <template>
-    <base-layout page-title="The Details" back-link="/memories">
+    <base-layout :page-title="loaded?loaded.title:'loading'" back-link="/memories">
        
             <ion-list>
-                <ion-item>One detail</ion-item>
+                <ion-item v-if="!loaded">not loaded</ion-item>
+                <ion-item v-else>{{ loaded.description }}</ion-item>
                 
             </ion-list>
       
@@ -10,14 +11,30 @@
     
 </template>
 <script>
+import { useMemoriesStore } from '@/store/index';
 import {IonList, IonItem} from "@ionic/vue";
 export default{
     components:{
      
         IonList, 
         IonItem
-    
-
-    },
+     },
+     data(){
+        return {
+            memoryId:this.$route.params.id 
+        };
+     },
+     computed:{
+        loaded(){
+            console.log(this.memoryId);
+            return useMemoriesStore().memory(this.memoryId);
+           
+        }
+     },
+     /*watch:{
+        $route(currentRoute){
+            this.memoryId=currentRoute.params.id
+        },
+     }*/
 }
 </script>
